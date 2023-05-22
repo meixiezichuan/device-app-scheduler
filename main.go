2c8b216ff67085f7f38c493c8d4a2e51d12110e4
meixiezichuan <meixiezichuan@163.com>
2023-05-22 15:59:55 +0800

    remove deafultbinder

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
+	"github.com/meixiezichuan/device-app-scheduler/networkaware"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/cmd/kube-scheduler/app"
-	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/defaultbinder"
-
-	"github.com/meixiezichuan/device-app-scheduler/networkaware"
 )
 
 func main() {
 	command := app.NewSchedulerCommand(
-		app.WithPlugin(defaultbinder.Name, defaultbinder.New),
 		app.WithPlugin(networkaware.NetworkOverheadName, networkaware.NewNetworkOverhead),
 	)
 
